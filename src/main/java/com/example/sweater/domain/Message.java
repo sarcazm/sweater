b1051223893d104a1d3a6583ab66639a4fb840b5
Nikolay Marchenko <a4241338a@yandex.ru>
2021-02-06 08:39:02 +0300

    Первый коммит.

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,29 @@
+package com.example.sweater.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+}
